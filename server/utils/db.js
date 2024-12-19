86be2aa6ae9dbe24c889e9bee006f83b4a35754a
silgenius <olutademartin@gmail.com>
2024-12-18 17:10:01 -0800

    feat(db): Improve db functionalities

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+
 require('dotenv').config();
 
 class DBClient {
@@ -10,61 +11,79 @@ class DBClient {
     this.dbClient = null;
     this.db = null;
     this.verifyConnection = false;
+    this.DBCollections = ['users', 'userFollows', 'blogs', 'conversations', 'notifications'];
+  }
+
+  async init() {
+    this.dbClient = new MongoClient(this.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    try {
+      await this.dbClient.connect();
+      this.verifyConnection = true;
+      this.db = this.dbClient.db(this.dbName);
+      console.log(`mongo successfully connected to ${this.dbName}`);
+    } catch (err) {
+      console.log('Mongo encountered an error');
     }
+  }
 
-    async init() {
-        this.dbClient = new MongoClient(this.url, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-      });
+  isAlive() {
+    return this.verifyConnection;
+  }
 
+  async insertData(collectionType, details) {
+    if (this.db) {
+      if (!this.DBCollections.includes(collectionType)) {
+        return new Error('Unkown collection type');
+      }
+      const collection = this.db.collection(collectionType);
       try {
-        await this.dbClient.connect();
-        this.verifyConnection = true;
-        this.db = this.dbClient.db(this.dbName);
-	console.log(`mongo successfully connected to ${this.dbName}`);
+        details.createdAt = new Date().toUTCString();
+        details.updatedAt = new Date().toUTCString();
+
+        const result = await collection.insertOne(details);
+        return result;
       } catch (err) {
-        console.log('Mongo encountered an error');
+        console(`Insert into ${collectionType} Error`);
       }
     }
+  }
 
-    isAlive() {
-        return this.verifyConnection;
-    }
+  async countCollection(collectionType) {
+    if (this.db) {
+      if (!collectionType) {
+        return new Error('Collection type not specified');
+      }
 
-    async createCollection(collectionType, details) {
-        if (this.db) {
-            const collection = this.db.collection(collectionType);
-            try {
-                const result = await collection.insertOne(details);
-                return result;
-            } catch (err) {
-                console(`Insert into ${collectionType} Error`)
-            }
-        }
-    }
+      const collection = this.db.collection(collectionType);
+      if (!collection) {
+        return new Error('Collection type does not exist');
+      }
 
-    async countCollection(collectionType) {
-        if (this.db) {
-            const collection = this.db.collection(collectionType)
-            const collectionCount = await collection.countDocuments();
-            return collectionCount;
-        }
+      const collectionCount = await collection.countDocuments();
+      return collectionCount;
     }
+  }
 
-    async findData(collectionType, details) {
-        if (this.db) {
-            const collection = this.db.collection(collectionType);
-            const result = collection.findOne(details);
-            return result;
-        }
+  async findData(collectionType, details) {
+    if (this.db) {
+      const collection = this.db.collection(collectionType);
+      if (!collection) {
+        return new Error('Collection type does not exist');
+      }
+      const result = collection.findOne(details);
+      return result;
     }
+  }
 }
 
 const dbClient = new DBClient();
 
 (async () => {
-    await dbClient.init();
+  await dbClient.init();
 })();
 
 module.exports = dbClient;
