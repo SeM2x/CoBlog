426e2b59c2d315ffcc2d3ba4bf89f4c875e90051
silgenius <olutademartin@gmail.com>
2024-12-20 14:14:48 -0800

    refactor: Restrict server to handle only /api routes

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
+import { userRouter, authRouter, router } from './routes/index';
+
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-import { userRouter, authRouter, router } from './routes/index';
 
 const app = express();
 
 app.use(express.json());
 
 const corsOptions = {
-	origin: 'http://localhost:3000'
-}
+  origin: 'http://localhost:3000',
+};
 app.use(cors(corsOptions));
 
-app.use('/users', userRouter);
-app.use('/auth', authRouter);
-app.use('/', router);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api', router);
 
 const server = http.createServer(app);
 
@@ -22,5 +23,5 @@ const hostname = '0.0.0.0';
 const port = '5000';
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
