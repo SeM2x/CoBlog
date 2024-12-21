92e3dc296f828f289a53d025517be33355647040
silgenius <olutademartin@gmail.com>
2024-12-21 15:19:47 -0800

    feat: Install and configure Morgan for HTTP request logging

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ import { userRouter, authRouter, router } from './routes/index';
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 const corsOptions = {
   origin: 'http://localhost:3000',
