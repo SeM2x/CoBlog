b4eb1f1956eaa33f22fbc2c8f957784b652597bc
SeM2x <houssem135.302@gmail.com>
2025-01-15 22:42:12 +0100

    fix: update socket URL and disable secure option

--- client/socket.ts
+++ client/socket.ts
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
 
 const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_SERVER || 'https://talkify.techerudites.tech';
+  process.env.NEXT_PUBLIC_SOCKET_SERVER || 'http://api.techerudites.tech:5000';
 
-const socket = io(SOCKET_URL);
+const socket = io(SOCKET_URL, { secure: false });
 
 export default socket;
