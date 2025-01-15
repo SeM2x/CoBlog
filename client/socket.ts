036848cd4e5bdf437f35e27e115fe808178bc230
SeM2x <houssem135.302@gmail.com>
2025-01-15 23:38:28 +0100

    fix: update socket URL to use HTTPS and remove secure option

--- client/socket.ts
+++ client/socket.ts
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
 
 const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_SERVER || 'http://api.techerudites.tech:5000';
+  process.env.NEXT_PUBLIC_SOCKET_SERVER || 'https://api.techerudites.tech';
 
-const socket = io(SOCKET_URL, { secure: false });
+const socket = io(SOCKET_URL);
 
 export default socket;
