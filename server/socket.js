6ce4bc1b8b77ce0fdddb76ea4fb46bc7f995e2de
silgenius <olutademartin@gmail.com>
2025-01-17 00:03:08 +0100

    fix: new update to fix socket bug

--- server/socket.js
+++ server/socket.js
@@ -4,15 +4,17 @@ const socketIo = require('socket.io');
 
 const socketConfig = {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
   },
 };
-const io = socketIo(server, socketConfig);
+export const io = socketIo(server, socketConfig);
 
 const hostname = '0.0.0.0';
 const port = '5000';
+export const socketArray = [];
 
 io.on('connection', (socket) => {
+  socketArray.push(socket);
   socket.on('send_message', (details) => {
     io.emit('message_sent', details);
   });
@@ -24,13 +26,8 @@ io.on('connection', (socket) => {
   socket.on('accept_invite', (details) => {
     io.emit('invite_accepted', details);
   });
-
-  socket.on('send_notification', (details) => {
-    io.emit('notification_sent', details);
-  });
 });
 
-module.exports = io;
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
