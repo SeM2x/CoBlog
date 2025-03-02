d31241f1f77af35f39b0da49d10e98ada7cc143a
silgenius <olutademartin@gmail.com>
2025-03-02 20:43:22 +0100

    feat: Implement function to generate OTP

--- server/utils/uuid.js
+++ server/utils/uuid.js
@@ -4,3 +4,9 @@ export default function generateId () {
   const newUuid = uuidv4();
   return newUuid;
 };
+
+
+export function generateOTP() {
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  return otp.toString();
+}
