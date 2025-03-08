d69681a49d939ea5dfaafab1f4b89556248a983f
silgenius <olutademartin@gmail.com>
2025-03-08 20:17:59 +0100

    refactor: Update for readability

--- storageServer/controller/FilesController.js
+++ storageServer/controller/FilesController.js
@@ -1,46 +1,10 @@
-import fileQueue from '../worker';
-
-const { ObjectId } = require('mongodb');
-const multer = require('multer');
-const path = require('path');
-
-const serverName = process.env.STORAGE_SERVER_URL;
-const { v4: uuidv4 } = require('uuid');
+const fs = require('fs').promises;
+const avatarUpload = require('utils/MulterConfig')
 const dbClient = require('../utils/storageDb');
+const serverName = process.env.STORAGE_SERVER_URL;
 
-// Multer config
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, '/home/ubuntu/storage');
-  },
-  filename(req, file, cb) {
-    const fname = `${file.fieldname}_${uuidv4()}${path.extname(file.originalname)}`;
-    req.localFileName = fname;
-    cb(null, fname);
-  },
-});
-
-const supportedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-const fileFilter = (req, file, cb) => {
-  console.log('In the filter');
-  if (!supportedFileTypes.includes(file.mimetype)) {
-    req.fileError = new Error('Unsupported file type');
-    cb(req.fileError, false);
-  } else {
-    cb(null, true);
-  }
-};
-
-const upload = multer(
-  {
-    storage,
-    limits: { fileSize: 1024 * 1024 * 2 },
-    fileFilter,
-  },
-).single('avatar');
-
-export function fileUpload(req, res) {
-  upload(req, res, async (err) => {
+export function UploadUserAvatar(req, res) {
+  avatarUpload(req, res, async (err) => {
     const userId = new ObjectId(req.user.userId);
     if (err instanceof multer.MulterError) {
       return res.status(400).json({ status: 'error', message: 'File must not exceed 2MB' });
@@ -58,10 +22,10 @@ export function fileUpload(req, res) {
       details.profileUrl = `${serverName}${req.localFileName}`;
 
       // Check if a user already have avatar uploaded
-      const search = await dbClient.findData('storage', { userId, fieldname: 'avatar' });
-      if (search) {
+      const query = await dbClient.findData('storage', { userId, fieldname: 'avatar' });
+      if (query) {
         await dbClient.deleteData('storage', { userId, fieldname: 'avatar' });
-        fileQueue.add({ filePath: search.path }); // Create a job to delete avatar in storage
+        await fs.unlink(query.localFileName) // delete existing file if exist
       }
 
       await dbClient.insertData('storage', details);
@@ -73,12 +37,3 @@ export function fileUpload(req, res) {
     }
   });
 }
-
-// Log job completion or failure
-fileQueue.on('failed', (job, err) => {
-  console.log(`Job ${job.id} failed with error: ${err.message}`);
-});
-
-fileQueue.on('completed', (job) => {
-  console.log(`Job ${job.id} completed`);
-});
